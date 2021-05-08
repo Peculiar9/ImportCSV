079230f2d1187b9bd701412afb22d77d215ee424
Peculiar <peculiarbabalola@gmail.com>
2021-05-08 19:53:49 +0100

    Added Starter FIles

---
+++ CSVUploadSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSVUploadSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
